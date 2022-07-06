a30b9a463664b40d0840ba804b9741295fbc7c2f
Eggrolling <34687658+Eggrolling-hu@users.noreply.github.com>
2022-07-06 10:27:18 +0800

    Revert "Add locate image area on image"
    This reverts commit 37df733d4d0f68696875301dbb21fd15d66384ae.

--- src/packages/image/Image.py
+++ src/packages/image/Image.py
@@ -4,26 +4,19 @@ Image class
 """
 
 # from src.packages.image.basic import Basic
-from robot.api.deco import keyword, library
+from robot.api.deco import keyword
 import pyscreeze
 
 
-@library
 class Image:
     def __init__(self) -> None:
         pass
 
     @keyword
-    def smoke(self, text: str):
-        print("Hello World", text)
+    def hello(self):
+        print("Hello World")
         return
 
     @keyword
-    def smoke_smoke_smoke_smoke_smoke(self, needleImage: str,
-                                      haystackImage: str):
-        print(needleImage, haystackImage)
-        return
-
-    @keyword
-    def locate(self, needleImage: str, haystackImage: str, **kwargs):
+    def locate_image_area_on_image(needleImage, haystackImage, **kwargs):
         return pyscreeze.locate(needleImage, haystackImage, **kwargs)
